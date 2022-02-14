22ed0f845f7da912a20575e0118e075023f306ef
陳宜庭 <studenttim@chenyitingdeMacBook-Pro-2.local>
2022-02-15 03:32:39 +0800

    chore: update .vscode debug config

--- mars_calculator/src/main.rs
+++ mars_calculator/src/main.rs
@@ -3,10 +3,18 @@ use std::io;
 fn main() {
     let mut input = String::new();
 
-    some_fn(&mut input);
+    let s1 = &input;
+    let s2 = &input;
 
+    print!("{}{}", s1, s2);
+
+    some_fn(&mut input);
+    
     io::stdin().read_line(&mut input);
 
+    borrow_string(&mut input);
+    own_string(input);
+    
     let mut mars_weight = calculate_weight_on_mars(100.0);
 
     mars_weight = mars_weight * 1000.0;
@@ -15,10 +23,19 @@ fn main() {
     // Ok(());
 }
 
+fn borrow_string(s: &String) {
+    print!("{}", s);
+}
+
+fn own_string(s: String) {
+    print!("{}", s);
+}
+
 fn calculate_weight_on_mars(weight: f32) -> f32 {
     (weight / 9.81) * 3.711
 }
 
 fn some_fn(s: &mut String) {
-    println!("{}?", *s);
+    s.push_str("pushed");
+    println!("{}", *s);
 }
